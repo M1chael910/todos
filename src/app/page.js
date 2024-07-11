7baef905255c0208ed802cb20fd6e75f2bdc2b1c
M1chael910 <mpmurphybusiness@gmail.com>
2024-07-10 23:51:27 -0500

    basic react project

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,29 @@
 import styles from "./page.module.css";
+function button() {
+  return <button>I'm Gay</button>;
+}
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={"todo"}>
         <p>todos</p>
+        <button />
       </div>
     </main>
   );
 }
+
+
+// function MyButton() {
+//   return <button>I'm a button</button>;
+// }
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+//     </div>
+//   );
+// }
